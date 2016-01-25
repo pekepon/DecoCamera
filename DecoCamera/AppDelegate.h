de27d27242042a290458501fcefc0c99f86acb10
井上圭一 <inoue@inter-reuse.co.jp>
2016-01-25 13:47:24 +0900

    Initial Commit

---
+++ DecoCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DecoCamera
+//
+//  Created by 井上圭一 on 2016/01/25.
+//  Copyright © 2016年 keiichi, inoue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DecoCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DecoCamera
+//
+//  Created by 井上圭一 on 2016/01/25.
+//  Copyright © 2016年 keiichi, inoue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
